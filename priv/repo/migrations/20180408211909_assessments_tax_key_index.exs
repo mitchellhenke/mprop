c29f62928d3869564ea85d836558799b20c32667
Mitchell Henke <mitchh23@gmail.com>
2018-04-08 16:22:45 -0500

    add tax_key index

---
+++ priv/repo/migrations/20180408211909_assessments_tax_key_index.exs
@@ -0,0 +1,8 @@
+defmodule Properties.Repo.Migrations.AssessmentsTaxKeyIndex do
+  use Ecto.Migration
+  @disable_ddl_transaction true
+
+  def change do
+    create index("assessments", [:tax_key], concurrently: true)
+  end
+end
