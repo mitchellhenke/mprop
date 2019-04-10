93bf5bca48148c930140e114ac8db91ce7dbbb9e
Mitchell Henke <mitchell@mitchellhenke.com>
2019-04-10 16:25:00 -0500

    play by my own javascript rules

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,7 @@
 import Leaflet from "leaflet"
 const pathname = window.location.pathname.slice(0, 2)
 
+if(pathname == "/" || pathname == "/?") {
 let map = L.map('map').setView([42.9994366888185, -87.8985320066727], 15);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -84,3 +85,5 @@ map.on('moveend', (e) => {
 })
 
 updateMap()
+
+}
