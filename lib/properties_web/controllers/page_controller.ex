cb39fe60e14b6ccd0534f2fef6aaa00bc30a5107
Mitchell Henke <mitchell@mitchellhenke.com>
2021-04-05 08:41:57 -0500

    dedup

--- lib/properties_web/controllers/page_controller.ex
+++ lib/properties_web/controllers/page_controller.ex
@@ -1,5 +1,6 @@
 defmodule PropertiesWeb.PageController do
   use PropertiesWeb, :controller
+  require Logger
   import Phoenix.HTML
 
   def index(conn, _params) do
@@ -7,18 +8,32 @@ defmodule PropertiesWeb.PageController do
   end
 
   def civ_webhook(conn, params) do
-    %{"value1" => game_name, "value2" => player_name, "value3" => turn_number} =
-      params
+    %{"value1" => game_name, "value2" => player_name, "value3" => turn_number} = params
 
     new_row = [player_name, turn_number, NaiveDateTime.utc_now()]
     existing_turns = existing_turns(game_name)
-    save_turns(game_name, [new_row | existing_turns])
+
+    is_duplicate_turn =
+      Enum.any?(existing_turns, fn turn ->
+        match?([^player_name, ^turn_number, _], turn)
+      end)
+
+    if is_duplicate_turn do
+      Logger.info("skipping duplicate for #{game_name}, #{player_name}, #{turn_number}")
+      save_turns("#{game_name}_backup", [new_row | existing_turns])
+    else
+      save_turns(game_name, [new_row | existing_turns])
+    end
 
     url = PropertiesWeb.Router.Helpers.page_url(conn, :civ_turns, %{game: game_name})
 
-    Properties.Twilio.send_message(
-      "It is #{player_name}'s turn in #{game_name}. The current turn number is #{turn_number}. See turns at #{url}."
-    )
+    unless is_duplicate_turn do
+      Properties.Twilio.send_message(
+        "It is #{player_name}'s turn in #{game_name}. The current turn number is #{turn_number}. See turns at #{
+          url
+        }."
+      )
+    end
 
     send_resp(conn, 201, "")
   end
@@ -27,93 +42,101 @@ defmodule PropertiesWeb.PageController do
     game_name = Map.get(params, "game", "My game")
     existing_turns = existing_turns(game_name)
     total_game_time = total_game_time(existing_turns)
-    existing_turns = existing_turns
-                     |> Enum.reverse()
-                     |> Enum.chunk_every(2, 1)
-                     |> Enum.reverse()
-                     |> Enum.map(fn([[player, turn, time] | next_turn]) ->
-                       turn_time = time_diff_seconds(time, next_turn)
-                       [player, turn, time, turn_time]
-                     end)
 
-    player_averages = Enum.group_by(existing_turns, fn([player, _turn, _time, _turn_time]) ->
-      player
-    end)
-    |> Enum.map(fn({player, turns}) ->
-      average_seconds = if Enum.count(turns) == 0 do
-        0
-      else
-        turn_times = Enum.map(turns, fn(turn) -> Enum.at(turn, 3) end)
-        (Enum.sum(turn_times) / Enum.count(turns))
-        |> round()
-      end
+    existing_turns =
+      existing_turns
+      |> Enum.reverse()
+      |> Enum.chunk_every(2, 1)
+      |> Enum.reverse()
+      |> Enum.map(fn [[player, turn, time] | next_turn] ->
+        turn_time = time_diff_seconds(time, next_turn)
+        [player, turn, time, turn_time]
+      end)
 
-      [player, average_seconds]
-    end)
-    html = ~E"""
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="utf-8"/>
-      <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
-    <body>
-      <h1>
-        <%= game_name %>
-      </h1>
-      <h2>
-        <%= total_game_time %>
-      </h2>
-      <table>
-          <thead>
-              <tr>
-                  <th>Player</th>
-                  <th>Turn</th>
-                  <th>Start Time</th>
-                  <th>Turn Length</th>
-              </tr>
-          </thead>
-          <tbody>
-            <%= Enum.map(existing_turns, fn([player, turn, time, turn_time_seconds]) -> %>
-              <tr>
-              <td><%= player %></td>
-              <td><%= turn %></td>
-              <td><%= format_naive_date_time(time) %></td>
-              <td><%= format_time_diff(turn_time_seconds) %></td>
-              </tr>
-            <% end) %>
-          </tbody>
-      </table>
-      <table>
-          <thead>
-              <tr>
-                  <th>Player</th>
-                  <th>Average Turn Length</th>
-              </tr>
-          </thead>
-          <tbody>
-            <%= Enum.map(player_averages, fn([player, average_turn_time_seconds]) -> %>
-              <tr>
-              <td><%= player %></td>
-              <td><%= format_time_diff(average_turn_time_seconds) %></td>
-              </tr>
-            <% end) %>
-          </tbody>
-      </table>
-    </body>
-    """
-    |> safe_to_string()
+    player_averages =
+      Enum.group_by(existing_turns, fn [player, _turn, _time, _turn_time] ->
+        player
+      end)
+      |> Enum.map(fn {player, turns} ->
+        average_seconds =
+          if Enum.count(turns) == 0 do
+            0
+          else
+            turn_times = Enum.map(turns, fn turn -> Enum.at(turn, 3) end)
+
+            (Enum.sum(turn_times) / Enum.count(turns))
+            |> round()
+          end
+
+        [player, average_seconds]
+      end)
+
+    html =
+      ~E"""
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </head>
+      <body>
+        <h1>
+          <%= game_name %>
+        </h1>
+        <h2>
+          <%= total_game_time %>
+        </h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Player</th>
+                    <th>Turn</th>
+                    <th>Start Time</th>
+                    <th>Turn Length</th>
+                </tr>
+            </thead>
+            <tbody>
+              <%= Enum.map(existing_turns, fn([player, turn, time, turn_time_seconds]) -> %>
+                <tr>
+                <td><%= player %></td>
+                <td><%= turn %></td>
+                <td><%= format_naive_date_time(time) %></td>
+                <td><%= format_time_diff(turn_time_seconds) %></td>
+                </tr>
+              <% end) %>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <th>Player</th>
+                    <th>Average Turn Length</th>
+                </tr>
+            </thead>
+            <tbody>
+              <%= Enum.map(player_averages, fn([player, average_turn_time_seconds]) -> %>
+                <tr>
+                <td><%= player %></td>
+                <td><%= format_time_diff(average_turn_time_seconds) %></td>
+                </tr>
+              <% end) %>
+            </tbody>
+        </table>
+      </body>
+      """
+      |> safe_to_string()
 
     html(conn, html)
   end
 
   def existing_turns(game_name) do
     data = Redix.command!(:redix, ["GET", "#{game_name}_game_data"])
+
     if data do
       :zlib.uncompress(data)
       |> :erlang.binary_to_term()
-      |> Enum.map(fn([player, turn, date_string]) ->
+      |> Enum.map(fn [player, turn, date_string] ->
         [player, turn, NaiveDateTime.from_iso8601!(date_string)]
       end)
     else
@@ -122,12 +145,13 @@ defmodule PropertiesWeb.PageController do
   end
 
   def save_turns(game_name, turns) do
-    turns_binary = turns
-                   |> Enum.map(fn([player, turn, time]) ->
-                     [player, turn, NaiveDateTime.to_iso8601(time)]
-                   end)
-                   |> :erlang.term_to_binary()
-                   |> :zlib.compress()
+    turns_binary =
+      turns
+      |> Enum.map(fn [player, turn, time] ->
+        [player, turn, NaiveDateTime.to_iso8601(time)]
+      end)
+      |> :erlang.term_to_binary()
+      |> :zlib.compress()
 
     Redix.command!(:redix, ["SET", "#{game_name}_game_data", turns_binary])
   end
@@ -160,23 +184,30 @@ defmodule PropertiesWeb.PageController do
   defp format_time_diff(total_seconds) do
     days = div(total_seconds, 86400)
 
-    hours = rem(total_seconds, 86400)
-            |> div(3600)
+    hours =
+      rem(total_seconds, 86400)
+      |> div(3600)
 
-    minutes = rem(total_seconds, 3600)
-              |> div(60)
-    seconds = rem(total_seconds, 3600)
-              |> rem(60)
+    minutes =
+      rem(total_seconds, 3600)
+      |> div(60)
+
+    seconds =
+      rem(total_seconds, 3600)
+      |> rem(60)
 
     cond do
       days > 0 ->
         "#{days} days, #{hours} hours, #{minutes} minutes"
+
       hours > 0 ->
         "#{hours} hours, #{minutes} minutes"
+
       minutes > 0 ->
         "#{minutes} minutes, #{seconds} seconds"
+
       true ->
-      "#{seconds} seconds"
+        "#{seconds} seconds"
     end
   end
 end
