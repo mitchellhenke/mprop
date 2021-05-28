e4424bd2ed809c43f34275cd94bb6a1bf1d172d6
Mitchell Henke <mitchell@mitchellhenke.com>
2021-05-28 11:38:27 -0500

    ok

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -4,23 +4,23 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const devMode = process.env.NODE_ENV !== 'production'
 
 module.exports = (env, options) => ({
   optimization: {
     minimizer: [
-      new TerserPlugin({cache: true, parallel: true, sourceMap: devMode}),
+      new TerserPlugin({ cache: true, parallel: true, sourceMap: false }),
       new OptimizeCSSAssetsPlugin({})
     ]
   },
   entry: {
-    './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
+    'app': glob.sync('./vendor/**/*.js').concat(['./js/app.js']),
+    'live': glob.sync('./vendor/**/*.js').concat(['./js/live.js'])
   },
   output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, '../priv/static/js')
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../priv/static/js'),
+    publicPath: '/js/'
   },
-  devtool: devMode ? 'source-map' : undefined,
   module: {
     rules: [
       {
@@ -32,15 +32,7 @@ module.exports = (env, options) => ({
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: devMode
-            }
-          }
-        ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   },
