d045bcd6c5b7bdd4f55b507fdc3f7141eae3338e
Mitchell Henke <mitchell@mitchellhenke.com>
2022-07-10 11:02:03 -0500

    delete unused

--- lib/properties_web/views/twitter_view.ex
+++
@@ -1,9 +0,0 @@
-defmodule PropertiesWeb.TwitterView do
-  use PropertiesWeb, :view
-
-  def render("crc.json", %{response_token: response_token}) do
-    %{
-      response_token: response_token
-    }
-  end
-end
